1cfa91fd9051c4105308693efee283ce0260052d
Marc R. Hoffmann <hoffmann@mountainminds.com>
2026-06-03 09:48:59 +0200

    Add basic test infrastructure

--- gir-validation/test/test.py
+++ gir-validation/test/test.py
@@ -2,22 +2,30 @@ import unittest
 from pathlib import Path
 
 from lxml import etree
+import yaml
 
 REPO_ROOT_DIR = Path(__file__).resolve().parents[2]
 REFDOCS_DIR = REPO_ROOT_DIR / "gir-validation" / "test" / "refdocs"
 SCHEMA_FILE = REPO_ROOT_DIR / "schemas" / "gir" / "globexml_v1.0.xsd"
+RULES_FILE = REPO_ROOT_DIR / "gir-validation" / "rules.yaml"
+
 
 class TestGirXmlValidation(unittest.TestCase):
 
-	def test_positive_xml_is_valid_against_globexml_schema(self):
+    @classmethod
+    def setUpClass(cls):
+        with RULES_FILE.open("r", encoding="utf-8") as f:
+            cls.rules = yaml.safe_load(f)
 
-		xml_schema = etree.XMLSchema(etree.parse(str(SCHEMA_FILE)))
-		xml_doc = etree.parse(str(REFDOCS_DIR / "positive.xml"))
+    def test_positive_xml_is_valid_against_globexml_schema(self):
 
-		is_valid = xml_schema.validate(xml_doc)
-		error_log = "\n".join(str(err) for err in xml_schema.error_log)
-		self.assertTrue(is_valid, f"XML failed schema validation:\n{error_log}")
+        xml_schema = etree.XMLSchema(etree.parse(str(SCHEMA_FILE)))
+        xml_doc = etree.parse(str(REFDOCS_DIR / "positive.xml"))
+
+        is_valid = xml_schema.validate(xml_doc)
+        error_log = "\n".join(str(err) for err in xml_schema.error_log)
+        self.assertTrue(is_valid, f"XML failed schema validation:\n{error_log}")
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
